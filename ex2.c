81962172d0f6b09477fd291a7ae79925727ef2d6
youneswolf <younesbellakrid@gmail.com>
2023-04-13 10:02:31 +0000

    ex22

--- ex2.c
+++ ex2.c
@@ -4,12 +4,15 @@ void ft_putchar(char c){
 write(1,&c,1); 
 }
 
-int main(){	
+void ft_alphabet(){	
 int i=65;
 while(i<91){
 ft_putchar(i);
 i++;
 }
-return 0;
 
 }
+int main(){
+ft_alphabet();
+return 0;
+	}
